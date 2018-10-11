43aa69adca21e02e073be6a87532b3ba5d5a5e09
jinaroo <sohnjin@gmail.com>
2018-10-11 10:20:13 -0400

    added prefab folder with plant and firefly prefabs; made lightShaderControl script; rearranged objects in scene

---
+++ DoubleJinWalkingSim/Assets/scripts/lightShaderControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightShaderControl : MonoBehaviour
+{
+
+	public GameObject plantLight;
+
+	// Use this for initialization
+	void Start () {
+		plantLight.SetActive(false);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Debug.Log("Player is in trigger!");
+			plantLight.SetActive(true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Debug.Log("Player is not in trigger!");
+			//plantLight.SetActive(false);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
