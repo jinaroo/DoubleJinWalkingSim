81ec721f5ed69a9e9f6bff4832f2c6ed5b9efd3e
jinaroo <sohnjin@gmail.com>
2018-10-18 11:17:43 -0400

    made new soundEffectController script and added musicNote collider

---
+++ DoubleJinWalkingSim/Assets/scripts/soundEffectController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundEffectController : MonoBehaviour
+{
+	public AudioSource musicNote;
+	private bool isFirst = true;
+
+	private void Start()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (isFirst && other.CompareTag("Player"))
+		{
+			isFirst = false;
+			musicNote.Play();
+		}
+	}
+}
