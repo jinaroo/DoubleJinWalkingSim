ce14bf9b542ac04be0ac53d0848ae38c25cdb0d5
jinaroo <sohnjin@gmail.com>
2018-11-01 11:41:17 -0400

    worked on audio mixer and snapshots

---
+++ DoubleJinWalkingSim/Assets/scripts/audiMixerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class audiMixerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//AudioMixer
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
