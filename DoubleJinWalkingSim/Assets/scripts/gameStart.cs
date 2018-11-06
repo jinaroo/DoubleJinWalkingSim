84d830e2efc03601c95ed222a83041602686a23e
wujin2hao <517871695@qq.com>
2018-11-05 21:24:51 -0500

    add a new title scene

--- DoubleJinWalkingSim/Assets/scripts/gameStart.cs
+++ DoubleJinWalkingSim/Assets/scripts/gameStart.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gameStart : MonoBehaviour {
+public class gameStart : MonoBehaviour
+{
 
+	public float ScanLineJitter;
+	public float VerticalJump;
+	public float ColorDrift;
+
+	public float time2Glitch;
+	public float timeFromGlitch2Load;
+	
+	private bool _clicked = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,7 +21,25 @@ public class gameStart : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//to start game
-		if (Input.GetKeyDown (KeyCode.Space))
-			UnityEngine.SceneManagement.SceneManager.LoadScene ("ArtStyleTest_Seperate");
+		if (!_clicked)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				_clicked = true;
+				StartCoroutine(startScene(time2Glitch, timeFromGlitch2Load));
+			}
+		}
+	}
+
+	IEnumerator startScene(float time1, float time2)
+	{
+		yield return new WaitForSeconds(time1);
+		Camera.main.GetComponent<Kino.AnalogGlitch>().scanLineJitter = ScanLineJitter;
+		Camera.main.GetComponent<Kino.AnalogGlitch>().verticalJump = VerticalJump;
+		Camera.main.GetComponent<Kino.AnalogGlitch>().colorDrift = ColorDrift;
+
+		yield return  new WaitForSeconds(time2);
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("ArtStyleTest_Seperate");
+
 	}
 }
