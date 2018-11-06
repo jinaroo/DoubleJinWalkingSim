26e131ce7e3f7d695c013b017349a79d7c33b34b
jinaroo <sohnjin@gmail.com>
2018-11-05 21:44:11 -0500

    small changes to build setting order and end screen

---
+++ DoubleJinWalkingSim/Assets/scripts/turnOffGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOffGame : MonoBehaviour {
+	//turns off game
+	void Update()
+	{
+		if (Input.GetKey("escape"))
+		{
+			Application.Quit();
+		}
+	}
+}
